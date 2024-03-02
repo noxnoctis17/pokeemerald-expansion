81acb472d9f13ef048a095134fbf153dc15e8a79
noxnoctis17 <petrsykora17@gmail.com>
2024-03-02 07:11:06 -0500

    missing comma oops

--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -5618,12 +5618,12 @@ static const struct TrainerMon sParty_Laura[] = {
 static const struct TrainerMon sParty_Cyndy1[] = {
     {
     .iv = TRAINER_PARTY_IVS(12, 12, 12, 12, 12, 12),
-    .lvl = 36
+    .lvl = 36,
     .species = SPECIES_MEDICHAM,
     },
     {
     .iv = TRAINER_PARTY_IVS(12, 12, 12, 12, 12, 12),
-    .lvl = 36
+    .lvl = 36,
     .species = SPECIES_HARIYAMA,
     }
 };
